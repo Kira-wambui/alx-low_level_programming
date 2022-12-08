4e6d2625d11049883fafda995a9828fe12870f92
Kira-wambui <nancykira21@gmail.com>
2022-12-08 09:55:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- *   followed by a new line, to the standaed error.
+ *   followed by a new line, to the standard error
  *   return: Always 0 (Success)
  */
 int main(void)
